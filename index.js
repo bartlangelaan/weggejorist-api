4b3e8d80e96817f0ce443750a5a5568a2962f97a
Bart Langelaan <langelaanbart@gmail.com>
2016-12-26 03:13:49 +0100

    Add simple webserver

--- index.js
+++ index.js
@@ -1,11 +1,23 @@
-const mongoose = require('mongoose');
-mongoose.connect('mongodb://localhost/weggejorist');
-
-const DumpertVideo = require('./DumpertVideo');
-const request = require('request-promise');
 
 const bluebird = require('bluebird');
+const mongoose = require('mongoose');
+mongoose.connect('mongodb://localhost/weggejorist');
+mongoose.Promise = bluebird.Promise;
+
+const DumpertVideo = require('./DumpertVideo');
+const DumpertComment = require('./DumpertComment');
+const request = require('request-promise');
+
 const parseXML = bluebird.promisify(require('xml2js').parseString);
+
+const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    DumpertComment.find({'deleted.deleted': true}).sort({'deleted.detectedAt': -1}).limit(200).exec().then(data => res.json(data))
+});
+
+app.listen(process.env.PORT || 3000);
 
 
 function getLatest() {
@@ -40,10 +52,10 @@ function getLatest() {
 
 function refresh() {
     getLatest()
-        .then(() => console.log('> Checked and inserterd the latest 25 video\'s'))
+        .then(() => console.log('> Checked and inserterd the latest 25 videos'))
         .then(() => DumpertVideo.find().sort({published: -1}).select('_id videoId secret').limit(60).exec())
         .then(videos => bluebird.map(videos, video => video.refreshComments()))
-        .then(() => console.log('> Checked and updated all comments of the latest 60 video\'s'))
+        .then(() => console.log('> Checked and updated all comments of the latest 60 videos'))
         .then(() => setTimeout(refresh, 1000 * 60));
 }
 
