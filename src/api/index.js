86bb73df5e4c5738bab497fb0b64ca4a3cf0b24b
Bart Langelaan <langelaanbart@gmail.com>
2017-01-12 10:39:16 +0100

    Change to /comments and add query parameters

--- src/api/index.js
+++ src/api/index.js
@@ -3,8 +3,28 @@ import DumpertComment from '../models/DumpertComment';
 
 const api = express();
 
-api.get('/deleted', (req, res) => {
-    DumpertComment.find({'deleted.deleted': true}).sort({'deleted.detectedAt': -1}).limit(200).exec().then(data => res.json(data))
+api.get('/comments', (req, res) => {
+    const query = {};
+
+    // /?deleted=true
+    // /?deleted=false
+    if(req.query.deleted !== undefined) {
+        query['deleted.deleted'] = req.query.deleted ? true : false;
+    }
+
+    // /?banned=true
+    // /?banned=false
+    if(req.query.banned !== undefined) {
+        query['deleted.banned'] = req.query.banned ? true : false
+    }
+
+    let sort = {published: -1};
+
+    if(req.query.sort == 'deleted'){
+        sort = {'deleted.detectedAt': -1};
+    }
+
+    DumpertComment.find(query).sort(sort).limit(200).exec().then(data => res.json(data))
 });
 
 export default api;
