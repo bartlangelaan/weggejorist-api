aacf99ae7fa0307f8cc4e3672aac2b0a3d2b3b3d
Bart Langelaan <langelaanbart@gmail.com>
2017-01-12 10:07:23 +0100

    Move api to /deleted

--- src/api/index.js
+++ src/api/index.js
@@ -3,7 +3,7 @@ import DumpertComment from '../models/DumpertComment';
 
 const api = express();
 
-api.get('/', (req, res) => {
+api.get('/deleted', (req, res) => {
     DumpertComment.find({'deleted.deleted': true}).sort({'deleted.detectedAt': -1}).limit(200).exec().then(data => res.json(data))
 });
 
