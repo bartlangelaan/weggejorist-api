2137a0235ad368230d2ead400ab65644e4964017
Bart Langelaan <langelaanbart@gmail.com>
2017-02-27 01:35:22 +0100

    Create the most awesome stats api that was ever made

--- src/api/index.js
+++ src/api/index.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import DumpertComment from '../models/DumpertComment';
+import DumpertVideo from '../models/DumpertVideo';
 import cors from 'cors';
 
 const api = express();
@@ -32,6 +33,85 @@ api.get('/comments', (req, res) => {
     // }
 
     DumpertComment.find(query).sort(sort).limit(200).populate('videoId', 'title').exec().then(data => res.json(data));
+});
+
+api.get('/stats', async (req, res) => {
+
+    const allVideos = await DumpertVideo.aggregate([
+        {
+            $group: {
+                _id: null,
+                views: { $sum: '$views'  },
+                comments: { $sum: '$comments'}
+            }
+        }
+    ]);
+
+    const videosPerMonth = await DumpertVideo.aggregate([
+        {
+            $group: {
+                _id: {year: {$year: '$published'}, month: {$month: '$published'}},
+                views: {$sum: '$views'},
+                comments: {$sum: '$comments'}
+            },
+        },
+        {
+            $sort: {
+                '_id.year': 1,
+                '_id.month': 1,
+            }
+        }
+    ]);
+
+    const commentsPerMonth = await DumpertComment.aggregate([
+        {
+            $group: {
+                _id: {year: {$year: '$published'}, month: {$month: '$published'}},
+                deleted: { $sum: 1 },
+                deleted_newbie: { $sum: {$cond : [ "$newbie", 1, 0 ] } },
+                deleted_banned: { $sum: {$cond : [ "$banned", 1, 0 ] } }
+            },
+        },
+        {
+            $sort: {
+                '_id.year': 1,
+                '_id.month': 1,
+            }
+        }
+    ]);
+
+    res.json({
+        stats: {
+            items: {
+                count: await DumpertVideo.count({}),
+                uploaded: parseInt((await DumpertVideo.aggregate([
+                    {$sort: {published: -1}},
+                    {$limit: 100},
+                    {$sort: {videoId: -1}},
+                    {$limit: 1}
+                ]).exec())[0].videoId.split('_')[0]),
+                views: allVideos[0].views,
+                per_month: videosPerMonth
+            },
+            comments: {
+                count: allVideos[0].comments,
+                count_since_newbies: await DumpertComment.count({published: {$gte: new Date('2016-10-29T15:14:17Z')}}),
+                deleted: {
+                    count: await DumpertComment.count({}),
+                    newbie: await DumpertComment.count({newbie: true}),
+                    banned: await DumpertComment.count({banned: true}),
+                    newbieAndBanned: await DumpertComment.count({newbie: true, banned: true})
+                },
+                per_month: commentsPerMonth,
+            },
+            worker: {
+                comments: {
+                    scannedVideos: await DumpertVideo.count({commentsLastScanned: {$exists: true}})
+                }
+            }
+        }
+    });
+
 });
 
 export default api;
